6d59f8f2310eff3cfbf601213e6650e7657ee6cd
Luca Lambiase <luca.lambia@gmail.com>
2024-01-19 00:30:29 +0100

    commenti

--- privata.php
+++ privata.php
@@ -1,35 +1,46 @@
 <?php
+//Definiamo le credenziali da usare per la connessione al DB
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "root");
 define("DB_NAME", "livecoding_mysqli");
 define("DB_PORT", 3306);
 
+//Predisponiamo le variabili da popolare
 $error = null;
 $loggedUser = null;
-$loginFailed = null;
 $db = null;
 $nome = null;
 $pass = null;
 
+//Se abbiamo ricevuto parametri GET, popoliamo le variabili
 if (isset($_GET["user"]) && isset($_GET["pass"])) {
     $nome = $_GET["user"];
     $pass = $_GET["pass"];
 }
 
+//Tentiamo la connessione al database (in $db) gestendo errori (in $error)
 try {
     $db = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 } catch (Exception $e) {
     $error = "Eccezione durante la connessione al DB: " . $e->getMessage();
 }
 
-if ($error == null && $nome && $pass) {
+//Se abbiamo ricevuto dati per fare login && la connessione al DB è ok
+//controlliamo i dati sul database
+if ($nome && $pass && $error == null) {
+    //Prepariamo la query
     $query = $db->prepare("SELECT * FROM utenti WHERE nome  = ? AND password = ?");
+    //Popoliamo i dati nella query
     $query->bind_param("ss", $nome, $pass);
+    //Eseguiamo la query
     $query->execute();
+    //Recuperiamo i risultati della query
     $db_result = $query->get_result();
 
+    //Se i risultati contengono almeno una riga
     if ($db_result->num_rows > 0) {
+        //Recuperiamo il primo record (in $loggedUser)
         $loggedUser = $db_result->fetch_assoc();
     }
 }
@@ -46,19 +57,20 @@ if ($error == null && $nome && $pass) {
 </head>
 
 <body>
+    <!-- Se abbiamo ricevuto credenziali corrette per un utente -->
     <?php if ($loggedUser) { ?>
         <h2>Benvenuto <?= $nome ?></h2>
-    <?php } else {
-        //header("Location: form.php");
-    ?>
+        <!-- Altrimenti -->
+    <?php } else { /* header("Location: form.php"); } */ ?>
+        <!-- Se abbiamo ricevuto credenziali, ma senza riscontro su DB  -->
         <?php if ($nome && $pass) { ?>
             <div class="alert alert-danger" role="alert">Login failed!</div>
         <?php } ?>
-
+        <!-- Se si è verificato un errore di connessione al DB -->
         <?php if ($error) { ?>
             <div class="alert alert-danger" role="alert"><?= $error ?></div>
         <?php } ?>
-
+        <!-- Form di login -->
         <div class="container">
             <div class="row">
                 <div class="col-12">
